8d9b24ef740ab5be57e52e6824140997c8fdf865
Md Aamir <mdcreator.2.0@gmail.com>
2026-05-25 15:59:24 +0530

    feat: create initial SideBar component structure

---
+++ lib/components/sidebar.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SideBar extends StatefulWidget {
+  const SideBar({super.key});
+
+  @override
+  State<SideBar> createState() => _SideBarState();
+}
+
+class _SideBarState extends State<SideBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
